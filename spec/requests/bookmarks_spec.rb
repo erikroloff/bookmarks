94b47ed023b333082aa218012e027b051a2ecc42
erikroloff <erikroloff@gmail.com>
2014-03-01 12:59:50 -0600

    Hopefully finished rspec testing

--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -8,4 +8,30 @@ describe "Bookmarks" do
       response.status.should be(200)
     end
   end
+
+  describe "Show Bookmarks" do
+  	it "should have the content" do
+  		visit '/bookmarks'
+  		expect(page).to have_content('Listing bookmarks')
+  	end
+  end
+
+  describe "Add Bookmark" do
+  	it "should have the content" do
+  		visit '/bookmarks/new'
+  		expect(page).to have_content('New bookmark')
+  	end
+  	it "should not create a bookmark with an invalid url" do
+  		visit '/bookmarks/new' 
+  		fill_in "Url", :with=>""
+  		click_button "Create Bookmark"
+  		expect(page).to have_content('Name is too short')
+  	end
+  	it "should not create a bookmark with an invalid name" do
+  		visit '/bookmarks/new'
+  		fill_in "Name", :with=>""
+  		click_button "Create Bookmark"
+  		expect(page).to have_content('Url is too short')
+  	end
+  end
 end
