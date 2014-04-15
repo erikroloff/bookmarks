f6dabfde44596bfc8336ce11f354a4fcf1797c06
erikroloff <erikroloff@gmail.com>
2014-04-15 16:24:18 -0500

    Added user model and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Bookmarks::Application.routes.draw do
   resources :bookmarks
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
