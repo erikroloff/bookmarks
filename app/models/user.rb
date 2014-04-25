834c9bf05976f3284c4a284589642c0559118c7f
erikroloff <erikroloff@gmail.com>
2014-04-25 16:19:52 -0500

    Got Sign up working

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,22 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   validates :real_name, length: { maximum: 75, minimum: 2 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
